7d69647d8af661eef39e6f816d5bd5f508e94e06
s33dd <dsuhoverikov@gmail.com>
2022-03-20 17:04:45 +0200

    added manual input

--- Input.cs
+++ Input.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public enum InputType {
   Manual = 0,
@@ -8,5 +9,30 @@ public enum InputType {
 
 namespace CipherLab {
   public static class Input {
+    public static List<string> GetText(List<string> text) {
+      if (text == null) { 
+        text = new List<string>();
+      }
+      text.Add(Console.ReadLine());
+      Console.WriteLine($"{Environment.NewLine}Press Enter to end input. Press any other key to continue.{Environment.NewLine}");
+      ConsoleKeyInfo pressedKey = Console.ReadKey();
+      Console.WriteLine(Environment.NewLine);
+      while (pressedKey.Key != ConsoleKey.Enter) {
+        text.Add(Console.ReadLine());
+        Console.WriteLine($"{Environment.NewLine}Press Enter to end input. Press any other key to continue.{Environment.NewLine}");
+        pressedKey = Console.ReadKey();
+        Console.WriteLine(Environment.NewLine);
+      }
+      int emptyCounter = 0;
+      foreach (string row in text) {
+        if (row.Trim() == "") {
+          emptyCounter++;
+        }
+      }
+      if (emptyCounter == text.Count) {
+        text = null;
+      }
+      return text;
+    }
   }
 }
